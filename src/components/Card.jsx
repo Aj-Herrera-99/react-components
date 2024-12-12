454d757e78f7a964dd882257ebff09087a636fe9
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-12 15:59:14 +0100

    animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({ titolo, contenuto, immagine }) {
     return (
         <>
             <div
-                className={`${style.card} shadow-lg shadow-stone-500 bg-white rounded-xl overflow-hidden max-w-[80vw] sm:max-w-[50vw] lg:max-w-[350px] `}
+                className={`${style.card} shadow-lg shadow-stone-500 bg-white rounded-xl overflow-hidden max-w-[80vw] sm:max-w-[50vw] lg:max-w-[350px] hover:scale-110 hover:rotate-6 hover:shadow-xl hover:shadow-zinc-700 transition-all`}
             >
                 <div className="h-3/5">
                     <img
@@ -17,9 +17,7 @@ function Card({ titolo, contenuto, immagine }) {
                 <div className="h-2/5 p-3 flex flex-col justify-between items-start gap-2">
                     <div className="line-clamp-5 text-left">
                         <h2 className="text-lg font-semibold">{titolo}</h2>
-                        <p className="my-2 leading-5">
-                            {contenuto}
-                        </p>
+                        <p className="my-2 leading-5">{contenuto}</p>
                     </div>
                     <Badge />
                 </div>
