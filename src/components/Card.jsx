ff7fe1eb6e5d8cb62e9e44003876a4afada2a4db
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-13 01:25:42 +0100

    prova con useState combinato al click su una card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import Badge from "./Badge";
 import style from "./Card.module.css";
 
 function Card({ titolo, contenuto, immagine }) {
+    // card states
+    const [modal, setModal] = useState(false);
+
+    // card click event
+    const handleCardClick = (e) => {
+        setModal(!modal);
+    };
+
+    // classes
+    const modalClass =
+        "fixed w-[70vw] md:w-[50vw] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-52 z-20";
+        
     return (
         <div
-            className={`shadow-lg shadow-stone-500 bg-white rounded-xl mx-2 my-4 w-full sm:w-[calc((100%-32px)/2)] lg:w-[calc((100%-48px)/3)] overflow-hidden hover:scale-110 hover:rotate-6 hover:shadow-xl hover:shadow-zinc-700 transition-all`}
+            onClick={handleCardClick}
+            className={`card w-full sm:w-[calc((100%-32px)/2)] lg:w-[calc((100%-48px)/3)] bg-white rounded-xl mx-2 my-4 overflow-hidden cursor-pointer shadow-lg shadow-stone-500  hover:scale-110 hover:rotate-6 hover:shadow-xl hover:shadow-zinc-700 transition-all ${
+                modal && modalClass
+            }`}
         >
             <div className="h-3/5 w-full">
                 <img
@@ -19,7 +35,9 @@ function Card({ titolo, contenuto, immagine }) {
                         <h2 className="text-lg font-semibold">{titolo}</h2>
                     </div>
                     <div className="line-clamp-1 ">
-                        <p className="hidden md:block my-1 leading-5">{contenuto}</p>
+                        <p className="hidden md:block my-1 leading-5">
+                            {contenuto}
+                        </p>
                     </div>
                 </div>
                 <Badge />
